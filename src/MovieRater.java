6eade244f5389b7695c5511866b638ebe66e7b86
zaza <zazo7@wp.pl>
2010-11-23 23:14:27 +0100

    code cleanup

--- src/MovieRater.java
+++ src/MovieRater.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,21 +20,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.html.HTMLAnchorElement;
-import org.w3c.dom.html.HTMLDivElement;
 import org.xml.sax.SAXException;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.ElementNotFoundException;
-import com.gargoylesoftware.htmlunit.Page;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
-import com.gargoylesoftware.htmlunit.html.HtmlButton;
 import com.gargoylesoftware.htmlunit.html.HtmlDivision;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
-import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlListItem;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
@@ -52,7 +45,7 @@ public class MovieRater {
 					+ " " + m.group(2));
 			m = p.matcher(result);
 		}
-		
+
 		result = result.toLowerCase();
 
 		result = result.replace("r5.line", "");
@@ -140,7 +133,7 @@ public class MovieRater {
 		result = result.replaceAll("\\s{2,}", " ");
 		return result.trim();
 	}
-	
+
 	public static Map<String, String[]> scanDirs(String rootDir, String subDirsToExclude) {
 		Map<String, String[]> result = new HashMap<String, String[]>();
 		
@@ -157,90 +150,50 @@ public class MovieRater {
 		}
 		return result;
 	}
-	
+
 	public static Map<String, Item> getMovieRatings(Map<String, String[]> dirs_hash, Map<String, Item> movies_cache) throws IOException, SAXException {
-		
+
 		Map<String, Item> movies_hash = new HashMap<String, Item>();
-		
+
 		for (Iterator<String> iterator = dirs_hash.keySet().iterator(); iterator.hasNext();) {
 			String category = iterator.next();
 			System.out.println("======================================> Processing category: "+category);
-			
+
 			String[] subdirs = dirs_hash.get(category);
 			for (int i = 0; i < subdirs.length; i++) {
 				//  default
 				Item item = new Item(subdirs[i], "", 0.0f, category);
-				
+
 				String subdir = extractMovieName(subdirs[i]);
 				System.out.print("Looking for '" + subdir + "'... ");
-				
+
 				if (movies_cache.containsKey(subdirs[i])) {
 					item = movies_cache.get(subdirs[i]);
 					System.out.print("[cache] ");
 				} else {
-					String encoded = URLEncoder.encode(subdir, "UTF-8");
-					
-//					HttpUnitOptions.setScriptingEnabled(false);
-//					HttpUnitOptions.setExceptionsThrownOnScriptError(false);
+					final WebClient webClient = new WebClient(BrowserVersion.FIREFOX_3);
+					webClient.setThrowExceptionOnScriptError(false);
+					webClient.setJavaScriptEnabled(false);
 
-//					WebConversation wc = new WebConversation();
-//					WebResponse   resp = wc.getResponse( "http://www.filmweb.pl/search/film?q=" + encoded);
-//					System.out.println(resp.getText());
-//					WebLink link = resp.getLinkWith("Przejdź do Filmwebu");
-//					if (link != null) {
-////						link.click();
-//					System.out.println("================ wait! ===========");
-//					try {
-//						Thread.sleep(17*1000);
-//					} catch (InterruptedException e) {
-//						// TODO Auto-generated catch block
-//						e.printStackTrace();
-//					}
-//						resp   = wc.getCurrentPage();
-//						System.out.println(resp.getText());
-//					}
-//					Document document = resp.getDOM();
-					
-					
-					   final WebClient webClient = new WebClient(BrowserVersion.FIREFOX_3);
-					   //.setsetCssErrorHandler
-					   webClient.setThrowExceptionOnScriptError(false);
-					   webClient.setJavaScriptEnabled(false);
+					HtmlPage page = webClient.getPage("http://www.filmweb.pl");
 
-//					    HtmlPage page = webClient.getPage("http://www.filmweb.pl/search/film?q=" + encoded);
-					   HtmlPage page = webClient.getPage("http://www.filmweb.pl");
-					   
-//					    System.out.println( page.asXml());
-					    HtmlAnchor link = null;
-					    try {
-//					     link = page.getAnchorByHref("http://www.filmweb.pl/search/film?q=" + encoded);
-					    	link = page.getAnchorByHref("http://www.filmweb.pl");
-					    } catch (ElementNotFoundException e) {
-					    	
-					    }
-					    if (link != null) {
-					    	System.err.println("================need to click");
-					    	page = link.click();
-					    	System.out.println("================ clcik! ===========");
-					    }
-//					    System.out.println( page.asText());
-//
-//					  final WebClient webClient = new WebClient(BrowserVersion.FIREFOX_3);
-//					  HtmlPage page = webClient.getPage("http://www.filmweb.pl/");
-					  HtmlForm searchForm = page.getForms().get(0);
-//					  searchForm.setTextContent(subdir);
-					  searchForm.getInputByName("q").setValueAttribute(subdir);
-//					  page = (HtmlPage) searchForm.getInputByValue("Sign In").click();
+					HtmlAnchor link = null;
+					try {
+						link = page.getAnchorByHref("http://www.filmweb.pl");
+					} catch (ElementNotFoundException e) {
 
-					  HtmlSubmitInput submit = (HtmlSubmitInput) searchForm.getByXPath("//input").get(1);
-					  page = submit.click();
-//					  System.out.println("search="+ page.asText());
-					
-//					DOMParser parser = new DOMParser();
-//					parser.parse("http://www.filmweb.pl/search/film?q=" + encoded);
+					}
+					if (link != null) {
+						System.err.println("================need to click");
+						page = link.click();
+						System.out.println("================ clcik! ===========");
+					}
+					HtmlForm searchForm = page.getForms().get(0);
+					searchForm.getInputByName("q").setValueAttribute(subdir);
+					HtmlSubmitInput submit = (HtmlSubmitInput) searchForm.getByXPath("//input").get(1);
+					page = submit.click();
+
 					List<Item> results = new ArrayList<Item>();
-//					search(parser.getDocument(), subdirs[i], category, results);
-//					search(document, subdirs[i], category, results);
 					search(page, subdirs[i], category, results);
 					Item it = findBestMatch(subdirs[i], results);
 					if (it != null)
@@ -250,10 +203,10 @@ public class MovieRater {
 				movies_hash.put(subdirs[i], item);
 			}
 		}
-		
+
 		return movies_hash;
 	}
-	
+
 	public static Item findBestMatch(String dir, List<Item> results) {
 		Item bestMatch = results.get(0);
 		for (Iterator<Item> iterator = results.iterator(); iterator.hasNext();) {
@@ -277,49 +230,44 @@ public class MovieRater {
 		}
 		return bestMatch;
 	}
-	
+
 	public static void search(HtmlPage page, String dir, String category, List<Item> results) {
 		HtmlElement ul = page.getElementById("searchFixCheck");
 		Iterable<HtmlElement> lis = ul.getChildElements();
 		for (Iterator<HtmlElement> iterator = lis.iterator(); iterator.hasNext();) {
 			HtmlElement n = iterator.next();
-			
-		if (n instanceof HtmlListItem) {
-			HtmlListItem listItem = (HtmlListItem) n;
-			Object node = listItem.getByXPath("//a[@class='searchResultTitle']").get(0);
-		
-		if (node instanceof HtmlAnchor) {
-			HtmlAnchor anchor = (HtmlAnchor) node;
-				Node li = anchor.getParentNode().getParentNode();
-				// TODO: convert to java
-//				osoba = li.search("span[text()='[osoba]']")[0]
-//				if (!osoba.nil?) # skip [osoba]
-//				   next
-//				end
-				String title = anchor.getTextContent().trim();
-				
-				List<?> searchResultRatings = listItem.getByXPath("//div[@class='searchResultRating']");
-				for (Iterator iterator2 = searchResultRatings.iterator(); iterator2
-						.hasNext();) {
-					Object object = (Object) iterator2.next();
-					
-//					Node liChild = liChildNodes.item(i);
-					if (object instanceof HtmlDivision ) {
-						HtmlDivision  div = (HtmlDivision) object;
-//						if (div.getClassName().equals("searchResultRating")) {
+
+			if (n instanceof HtmlListItem) {
+				HtmlListItem listItem = (HtmlListItem) n;
+				Object node = listItem.getByXPath("//a[@class='searchResultTitle']").get(0);
+
+				if (node instanceof HtmlAnchor) {
+					HtmlAnchor anchor = (HtmlAnchor) node;
+					Node li = anchor.getParentNode().getParentNode();
+					// TODO: convert to java
+					// osoba = li.search("span[text()='[osoba]']")[0]
+					// if (!osoba.nil?) # skip [osoba]
+					// next
+					// end
+					String title = anchor.getTextContent().trim();
+
+					List<?> searchResultRatings = listItem.getByXPath("//div[@class='searchResultRating']");
+					for (Iterator it2 = searchResultRatings.iterator(); it2.hasNext();) {
+						Object object = (Object) it2.next();
+						if (object instanceof HtmlDivision ) {
+							HtmlDivision  div = (HtmlDivision) object;
 							Node span = div.getChildNodes().item(1);
 							String text = span.getTextContent();
 							Pattern p = Pattern.compile("(ocena: )([0-9]{1}\\.[0-9]{1,2})");
 							Matcher matcher = p.matcher(text);
 							if (matcher.find())
 								results.add(new Item(dir, title, Float.parseFloat(matcher.group(2)), category));
-//						}
+						}
 					}
+				}
 			}
 		}
-		}
-		}
-    }
+	}
 
 	private static Map<String, Item> sortByValue(Map<String, Item> map) {
 		List<Item> list = new LinkedList(map.entrySet());
@@ -335,7 +283,7 @@ public class MovieRater {
 		}
 		return result;
 	}
-	
+
 	public static void writeSortedItemsToFile(Map<String, Item> sortedItems, String filePath) throws IOException {
 		BufferedWriter out = new BufferedWriter(new FileWriter(filePath));
 		for (Iterator<Item> it = sortedItems.values().iterator(); it.hasNext();) {
@@ -345,7 +293,7 @@ public class MovieRater {
 		}
 		out.close();
 	}
-	
+
 	public static Map<String, Item> readCache(String root) throws IOException {
 		String filePath = findLatestCacheFile(root);
 		Map<String, Item> result = new HashMap<String, Item>();
@@ -354,17 +302,17 @@ public class MovieRater {
 			return result;
 		System.out.println("Found cache file:" + filePath);
 		FileReader fr = new FileReader(filePath);
-	    BufferedReader br = new BufferedReader(fr);
-	    String s;
-	    while ((s = br.readLine()) != null) {
-	    	Item i = Item.toItem(s);
-	    	if (i.rating > 0)
-	    		result.put(i.dir, i);
-	    }
-	    fr.close();
-	    return result;
+		BufferedReader br = new BufferedReader(fr);
+		String s;
+		while ((s = br.readLine()) != null) {
+			Item i = Item.toItem(s);
+			if (i.rating > 0)
+				result.put(i.dir, i);
+		}
+		fr.close();
+		return result;
 	}
-	
+
 	public static String findLatestCacheFile(String root) {
 		File rootFile = new File(root);
 		if (rootFile.isDirectory()) {
@@ -380,7 +328,7 @@ public class MovieRater {
 		}
 		return root + "//" + "test.txt";
 	}
-	
+
 	public static void main(String[] args) throws IOException, SAXException {
 		Map<String, String[]> dirs_hash = scanDirs(args[0], args[1]);
 		System.out.println("categories=" + dirs_hash.size());
